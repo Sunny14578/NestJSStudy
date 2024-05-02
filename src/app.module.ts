c3540966d7caad9b9eb980c9f1de66137700fdaa
choetaeyang <imsun0101@naver.com>
2024-05-03 04:08:35 +0900

    [feat]: login 기능 및 env 환경설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,32 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { validationSchema } from './config/validation.schema';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost', 
-    port: 5432,
-    username: 'nestUser',
-    password: 'nestPassword',
-    database: 'nestjsDB',
-    autoLoadEntities: true,
-    synchronize: true, 
-  }), 
+    ConfigModule.forRoot({
+      envFilePath: 'setting.env',
+      validationSchema,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'), 
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true, 
+      }),
+
+    }),
+    
   AuthModule],
 
   controllers: [AppController],
